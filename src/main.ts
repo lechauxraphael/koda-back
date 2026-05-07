c95b2e758a18817d44ee8d0494d4e246de190f69
LechauxRaphael <raphael.lechaux@my-digital-school.org>
2026-05-07 14:03:30 +0200

    fix: bug

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import 'dotenv/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3001);
   
 }
 bootstrap();
