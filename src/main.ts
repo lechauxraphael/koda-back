2c1fbe8339e9e70637f7a29f5849799f36a02264
LechauxRaphael <raphael.lechaux@my-digital-school.org>
2026-05-29 10:02:44 +0200

    feat: link front and back

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,16 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const frontendOrigins = (
+    process.env.FRONTEND_ORIGINS ??
+    'http://localhost:5500,http://127.0.0.1:5500,http://localhost:8080,http://127.0.0.1:8080,http://localhost:5173,http://127.0.0.1:5173'
+  )
+    .split(',')
+    .map((origin) => origin.trim())
+    .filter(Boolean);
 
   app.enableCors({
-    origin: ['http://localhost:5500', 'http://127.0.0.1:5500'],
+    origin: frontendOrigins,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
   });
